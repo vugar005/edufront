4183440acb29bb21f0c134367072d098c8616ca6
vugar005 <vugar_abdullayev@outlook.com>
2020-05-29 10:58:47 +0300

    develop

--- src/app/auth/password-recovery-email-sent/password-recovery-email-sent.component.ts
+++ src/app/auth/password-recovery-email-sent/password-recovery-email-sent.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { PASSWORD_RECOVER_EMAIL_SENT_CONSTANTS } from './password-recovery-email-sent.constants';
 
 @Component({
   selector: 'app-password-recovery-email-sent',
   templateUrl: './password-recovery-email-sent.component.html',
-  styleUrls: ['./password-recovery-email-sent.component.scss']
+  styleUrls: ['./password-recovery-email-sent.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PasswordRecoveryEmailSentComponent implements OnInit {
-
+  public constants = PASSWORD_RECOVER_EMAIL_SENT_CONSTANTS;
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/auth/password-recovery-email-sent/password-recovery-email-sent.constants.ts
@@ -0,0 +1,4 @@
+export const PASSWORD_RECOVER_EMAIL_SENT_CONSTANTS = {
+    title: 'We sent you an Email',
+    subtitle: `An Email has been sent to your registered email address, click on the link to reset your password.`,
+}
